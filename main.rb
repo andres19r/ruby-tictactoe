2e0f740c558ba9ec3abab8e480af6a17e8e796fa
Andres Rivero <andres19rivero@gmail.com>
2021-07-23 18:22:59 -0400

    feat: Add start_game method and players attribs

--- main.rb
+++ main.rb
@@ -20,15 +20,33 @@ class Game
     @player1 = player1
     @player2 = player2
   end
+  def start_game
+    puts "Select a number:"
+    puts "     |     |     "
+    puts "   1 |  2  |  3  "
+    puts " ----|-----|-----"
+    puts "   4 |  5  |  6  "
+    puts " ----|-----|-----"
+    puts "   7 |  8  |  9  "
+    puts "     |     |     "
+    option = gets.chomp
+  end
 end
 
 class Player
   attr_accessor :name, :mark
-  def initialize(name, mark)
+  def initialize
+    puts "Insert the name of the player:"
+    name = gets.chomp
     @name = name
+    puts "Insert your mark:"
+    mark = gets.chomp
     @mark = mark
   end
 end
 
 game = Game.new
+p1 = Player.new
+p p1.name
+p p1.mark
 game.show_grid
