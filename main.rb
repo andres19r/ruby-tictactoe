c9ab9fc5787b1a0ec3da1273e16a3f1c5e0b56f0
Andres Rivero <andres19rivero@gmail.com>
2021-07-24 01:13:14 -0400

    feat: set gaming rules and functionality

--- main.rb
+++ main.rb
@@ -9,11 +9,32 @@ class Game
   def start_game
     p1 = Player.new
     p1.mark = 'X'
-    puts "Your mark is #{p1.mark}"
+    puts "Your mark is '#{p1.mark}'"
     p2 = Player.new
     p2.mark = 'O'
-    puts "Your mark is #{p2.mark}"
+    puts "Your mark is '#{p2.mark}'"
     add_players(p1, p2)
+    sw = 1
+    until p1_winner? || p2_winner? || full_grid?
+      if sw == 1
+        p1.selection = choose(p1)
+        paint_grid(p1, p1.selection)
+        show_grid
+        sw = 2
+      elsif sw == 2
+        p2.selection = choose(p2)
+        paint_grid(p2, p2.selection)
+        show_grid
+        sw = 1
+      end
+    end
+    if p1_winner?
+      puts "Congrats #{p1.name}!!! You win!!!"
+    elsif p2_winner?
+      puts "Congrats #{p2.name}!!! You win!!!"
+    elsif full_grid?
+      puts "Owwww :( It's a draw! "
+    end
   end
   private
   def add_players(player1, player2)
@@ -21,7 +42,7 @@ class Game
     @player2 = player2
   end
   def choose(player)
-    puts "Select a number:"
+    puts "#{player.name} select a number:"
     puts "     |     |     "
     puts "   1 |  2  |  3  "
     puts " ----|-----|-----"
@@ -29,6 +50,20 @@ class Game
     puts " ----|-----|-----"
     puts "   7 |  8  |  9  "
     puts "     |     |     "
+    gets.chomp
+  end
+  def paint_grid(player, place)
+    case place
+    when "1" then @grid["row1"][0] = player.mark
+    when "2" then @grid["row1"][1] = player.mark
+    when "3" then @grid["row1"][2] = player.mark
+    when "4" then @grid["row2"][0] = player.mark
+    when "5" then @grid["row2"][1] = player.mark
+    when "6" then @grid["row2"][2] = player.mark
+    when "7" then @grid["row3"][0] = player.mark
+    when "8" then @grid["row3"][1] = player.mark
+    when "9" then @grid["row3"][2] = player.mark
+    end
   end
   def show_grid
     puts "     |     |     "
@@ -81,10 +116,25 @@ class Game
       false
     end
   end
+  def full_grid?
+   if @grid["row1"][0] != " " &&
+      @grid["row1"][1] != " " &&
+      @grid["row1"][2] != " " &&
+      @grid["row2"][0] != " " &&
+      @grid["row2"][1] != " " &&
+      @grid["row2"][2] != " " &&
+      @grid["row3"][0] != " " &&
+      @grid["row3"][1] != " " &&
+      @grid["row3"][1] != " "
+     true
+   else
+     false
+   end
+  end
 end
 
 class Player
-  attr_accessor :name, :mark
+  attr_accessor :name, :mark, :selection
   def initialize
     puts "Insert the name of the player:"
     name = gets.chomp
